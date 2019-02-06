96ccfe89cdff8995ec5d529a7db82ab1a35a28d0
Kiszczomb <kacper.kishchomb@gmail.com>
2019-02-06 23:23:53 +0100

    Change character set to utf8mb4; Connect to and get data from DB;

--- index.php
+++ index.php
@@ -10,37 +10,41 @@
 </head>
 <body>
     <h1>Newsletter</h1>
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "ZAQ!2wsx";
-$dbname = "newsdb";
+$mysqli = new mysqli("localhost", "root", "ZAQ!2wsx", "newsdb");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n<br/>", mysqli_connect_error());
+    exit();
+}
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully <hr>";
+printf("Initial character set: %s \n<br/>", $mysqli->character_set_name());
 
-// Get data from database
+/* change character set to utf8mb4 */
+if (!$mysqli->set_charset("utf8mb4")) {
+    printf("Error loading character set utf8: %s \n<br/>", $mysqli->error);
+    exit();
+} else {
+    printf("Current character set: %s \n<br/>", $mysqli->character_set_name());
+}
+
+/* make SQL request*/
 $sql = "SELECT id, title, content FROM news";
-$result = $conn->query($sql);
+$result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
+    /* output data of each row */
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. "<br>Tytuł: " . $row["title"]. "<br>Treść: " . $row["content"]. "<br>";
+        echo "id: " . $row["id"]. "<br> Tytuł: " . $row["title"]. "<br> Treść:  " . $row["content"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 
-$conn->close();
-
+$mysqli->close();
 ?>
-    
+
 </body>
 </html>
